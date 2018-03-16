fabefed5efe6dee0358b12eecf201826fc590d4a
maksym0920 <maksym0920@gmail.com>
2018-03-16 19:18:51 +0200

    new Controller

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,24 @@
 |
 */
 
+use Illuminate\Http\Request;
+
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/test1', function () {
+  return "test1";
+});
+
+Route::get('/testPost', function (Request $request) {
+  var_dump($request->all());
+  var_dump($request['a']);
+  var_dump($request['b']);
+
+
+  return "end";
+});
+
+Route::match(['get', 'post'],'/test3', function (Request $request) {
+  return 'test3';
 });
